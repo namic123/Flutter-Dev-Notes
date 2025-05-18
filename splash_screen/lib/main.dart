09813e17978dead87047491cae9b4fc8f5d9b07e
jspark <namic123@naver.com>
2025-05-19 02:03:09 +0900

    사용자 정의 위젯 작성해보기 (Vue로 치면 사용자 정의 컴포넌트 작성)

--- splash_screen/lib/main.dart
+++ splash_screen/lib/main.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-    backgroundColor: Color(0xFF335CB0),
-    body: Column(
-      mainAxisAlignment: MainAxisAlignment.center, // Css에서의 Align Items와 같은 속성
-      children: [
-        Image.asset('asset/img/logo.png'),
-        CircularProgressIndicator(
-          color: Colors.white
-        ),
-      ],
+  runApp(
+    MaterialApp(
+            // 사용자 정의 위젯
+      home: HomeScreen(),
     ),
-  )));
+  );
+}
+
+/// StatelessWidget (컴포넌트로 분리)
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF335CB0),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        // Css에서의 Align Items와 같은 속성
+        children: [
+          Image.asset('asset/img/logo.png'),
+          CircularProgressIndicator(color: Colors.white),
+        ],
+      ),
+    );
+  }
 }
