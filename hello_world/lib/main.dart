80b2568d13b647ca6d08f9fb4b32ca2abf416067
jspark <namic123@naver.com>
2025-05-19 00:23:29 +0900

    애뮬레이터 상단 우측에 Debug 표시 제거하는 방법

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     // Flutter에서 화면에 보여주는 무언가를 모두 위젯이라고 부름
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.black,
         body: Center(
