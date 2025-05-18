2497ec8eaa6f119f2c521318e9e5b0d2c28b16af
jspark <namic123@naver.com>
2025-05-19 02:26:17 +0900

    Padding 적용해보기 - HotReload 앱 변경사항 리로드 (현재 상태 유지하면서)

--- splash_screen/lib/main.dart
+++ splash_screen/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-            // 사용자 정의 위젯
+      // 사용자 정의 위젯
       home: HomeScreen(),
     ),
   );
@@ -17,17 +17,19 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF335CB0),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        // Css에서의 Align Items와 같은 속성
-        children: [
-          Image.asset('asset/img/logo.png'),
-          CircularProgressIndicator(color: Colors.white),
-        ],
-      ),
-    );
+        backgroundColor: Color(0xFF335CB0),
+        body: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 36.0,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // Css에서의 Align Items와 같은 속성
+            children: [
+              Image.asset('asset/img/logo.png'),
+              CircularProgressIndicator(color: Colors.white),
+            ],
+          ),
+        ));
   }
 }
-
-
