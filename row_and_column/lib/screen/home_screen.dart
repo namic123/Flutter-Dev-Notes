930f8a2249b7d91bf5ec98fafcb7f39c3e20b6ea
jspark <namic123@naver.com>
2025-05-23 23:58:45 +0900

    앱의 기본 틀이 되는 Container 위젯 사용하기

--- row_and_column/lib/screen/home_screen.dart
+++ row_and_column/lib/screen/home_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:row_and_column/const/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+
+      ),
+    );
   }
 }
