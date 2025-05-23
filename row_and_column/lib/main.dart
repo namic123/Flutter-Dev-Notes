e768b78a32f6327b630bbc57153c2b00f85023bb
jspark <namic123@naver.com>
2025-05-23 23:56:01 +0900

    앱을 만들기 위한 기본 위젯 작성

--- row_and_column/lib/main.dart
+++ row_and_column/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:row_and_column/screen/home_screen.dart';
 
-
 void main() {
-  runApp(HomeScreen());
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    )
+  );
 }
