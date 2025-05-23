6d6562995ed95672a52f63f7aab59aae979e74be
jspark <namic123@naver.com>
2025-05-24 00:15:35 +0900

    Row MainAxisAlignment 사용 - justify-content와 동일

--- row_and_column/lib/screen/home_screen.dart
+++ row_and_column/lib/screen/home_screen.dart
@@ -10,7 +10,7 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
         child: Container(
           color: Colors.black,
-          child: Column(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
               children: colors
                   .map((e) => Container(
